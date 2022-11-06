1f0daf0302bff447c2061533c6095b9a10992796
Светлана Чернова <lana_19.87@mail.ru>
2022-11-06 19:24:50 +0600

    Task1,Task2

---
+++ src/test/java/SelenideTest.java
@@ -0,0 +1,67 @@
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class SelenideTest {
+
+    @BeforeEach
+    public void openForm() {
+        open("http://localhost:9999");
+    }
+
+    @Test
+    void TestSelenideSuccess() {
+        $("[data-test-id=name] input").sendKeys("Иван Петров");
+        $("[data-test-id=phone] input").sendKeys("+79012345678");
+        $("[data-test-id=agreement]").click();
+        $("[type=button").click();
+        $("[data-test-id=order-success]").shouldHave(Condition.text(" Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+
+    @Test
+    void TestSelenideNameLat() {
+        $("[data-test-id=name] input").sendKeys("Ivan Petrov");
+        $("[data-test-id=phone] input").sendKeys("+79032348679");
+        $("[data-test-id=agreement]").click();
+        $("[type=button").click();
+        $("[data-test-id=name].input_invalid .input__sub").shouldHave(Condition.text("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+
+    @Test
+    void TestSelenidPhone() {
+        $("[data-test-id=name] input").sendKeys("Иван-Ваня Петров");
+        $("[data-test-id=phone] input").sendKeys("879032348679");
+        $("[data-test-id=agreement]").click();
+        $("[type=button").click();
+        $("[data-test-id=phone].input_invalid .input__sub").shouldHave(Condition.text("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
+
+    @Test
+    void TestSelenidClick() {
+        $("[data-test-id=name] input").sendKeys("Иван-Ваня Петров");
+        $("[data-test-id=phone] input").sendKeys("+79032348679");
+        $("[type=button").click();
+        $("[data-test-id=agreement].input_invalid .checkbox__text").shouldHave(Condition.text("Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй"));
+    }
+
+    @Test
+    void TestSelenideNameEmpty() {
+        $("[data-test-id=name] input").sendKeys("");
+        $("[data-test-id=phone] input").sendKeys("+79032348679");
+        $("[data-test-id=agreement]").click();
+        $("[type=button").click();
+        $("[data-test-id=name].input_invalid .input__sub").shouldHave(Condition.text("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    void TestSelenidPhoneEmpty() {
+        $("[data-test-id=name] input").sendKeys("Иван-Ваня Петров");
+        $("[data-test-id=phone] input").sendKeys("");
+        $("[data-test-id=agreement]").click();
+        $("[type=button").click();
+        $("[data-test-id=phone].input_invalid .input__sub").shouldHave(Condition.text("Поле обязательно для заполнения"));
+    }
+}
